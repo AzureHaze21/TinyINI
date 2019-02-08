9e724e9185aa4d31b7bd62cd533274975f001aaa
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2019-02-08 23:42:43 +0100

    Update TinyINI.h

--- TinyINI.h
+++ TinyINI.h
@@ -1,43 +1,87 @@
 #include <fstream>
+#include <iostream>
+#include <string>
 #include <map>
 #include <algorithm>
+
+enum Encoding
+{
+	DEFAULT,
+	UTF16LE,
+	UTF16BE,
+	UTF32LE,
+	UTF32BE,
+	UTF1,
+	UTFEBCDIC,
+	SCSU,
+	BOCU1,
+	GB18030
+};
 
 class TinyIni
 {
 private:
+	template<typename charType>
+	static Encoding ConsumeBOM(std::basic_ifstream<charType>& stream)
+	{
+		charType c = stream.peek();
+		if (c == 0xff) stream.get();
+		c = stream.peek();
+		if (c == 0xfe)
+		{
+			stream.get();
+			return UTF16LE;
+		}
+		return DEFAULT;
+	}
+	
+	static std::wstring& utf16le_to_utf8(std::wstring& s)
+	{
+		std::wstring cpy(L"");
+		for (size_t i{ 0 }; i < s.size(); i += 2)
+			cpy += s[i] | (s[i + 1] << 8);
+		s = cpy;
+		return s;
+	}
+
 	static std::wstring& TrimString(std::wstring& s)
 	{
-		s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](char c) { return c != ' '; }));
-		s.erase(std::find_if(s.rbegin(), s.rend(), [](char c) { return c != ' '; }).base(), s.end());
+		s.erase(s.begin(), std::find_if_not(s.begin(), s.end(), iswspace));
+		s.erase(std::find_if_not(s.rbegin(), s.rend(), iswspace).base(), s.end());
 		return s;
 	}
 
 	static std::pair<std::wstring, std::wstring> SplitKeyValues(std::wstring& line)
 	{
 		std::wstring key, val;
-		TrimString(key = line.substr(0, line.find('=')));
-		TrimString(val = line.substr(line.find('=') + 1, line.size()));
+		TrimString(key = line.substr(0, line.find(L'=')));
+		TrimString(val = line.substr(line.find(L'=') + 1, line.size()));
 		return  std::pair<std::wstring, std::wstring>{ key, val };
 	}
 public:
+	using KeyValues = std::map<std::wstring, std::wstring>;
+	using Sections = std::map<std::wstring, KeyValues>;
+	using IniFile = Sections;
+
 	static auto Read(const char *path)
 	{
-		using KeyValues = std::map<std::wstring, std::wstring>;
-		using Sections = std::map<std::wstring, KeyValues>;
 		Sections sections;
-		std::wstring currentSection = L"";
-		if (std::wifstream ifs(path); ifs)
+		std::wstring currentSection;
+		if (std::wifstream ifs(path, std::ios::binary); ifs)
 		{
+			Encoding encoding = ConsumeBOM(ifs);
 			std::wstring line;
 			while (std::getline(ifs, line))
 			{
-				if (line.empty()) continue;
-				// trim both ends of the string
+				if (line.empty() || std::all_of(line.begin(), line.end(), [](wchar_t c) { return iswspace(c) || c == 0; })) 
+					continue;
+				if (line.front() == 0) line.erase(line.begin());		
+				if (encoding == UTF16LE) utf16le_to_utf8(line);
 				TrimString(line);
-				if (line.front() == ';') continue;
-				if (line.front() == '[')
+				if (line.front() == L';') continue;
+				if (line.front() == L'[')
 				{
-					if (line.back() != ']') continue;
+					if (line.back() != L']') continue;
 					line.erase(line.begin());
 					line.erase(line.end() - 1);
 					TrimString(line);
@@ -47,7 +91,7 @@ public:
 				}
 				if (!currentSection.empty())
 				{
-					if (std::find(line.begin(), line.end(), '=') == line.end()) continue;
+					if (std::find(line.begin(), line.end(), L'=') == line.end()) continue;
 					auto kv = SplitKeyValues(line);
 					sections[currentSection].try_emplace(kv.first, kv.second);
 				}
