cc88520900b4ae43b77fc84a62beab4eb6943199
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2019-02-09 14:31:45 +0100

    Update TinyINI.h

--- TinyINI.h
+++ TinyINI.h
@@ -9,6 +9,7 @@
 enum Encoding
 {
 	DEFAULT,
+	UTF8,
 	UTF16LE,
 	UTF16BE,
 	UTF32LE,
@@ -20,32 +21,36 @@ enum Encoding
 	GB18030
 };
 
+template<typename charType>
+Encoding ConsumeBOM(std::basic_ifstream<charType>& stream)
+{
+	unsigned char BOM[4]{ 0 };
+	charType c = 0;
+	int i = 0;
+	while (stream && (i < sizeof(BOM)))
+	{
+		BOM[i++] = stream.peek();
+		stream.get();
+		if (BOM[0] == 0xFF && BOM[1] == 0xFE) return UTF16LE;
+		if (BOM[0] == 0xFE && BOM[1] == 0xFF) return UTF16BE;
+		if (BOM[0] == 0xEF && BOM[1] == 0xBB && BOM[2] == 0xBF) return UTF8;
+	}
+	stream.seekg(0, std::ios::beg);
+	return DEFAULT;
+}
+
+std::wstring& utf16le_to_utf8(std::wstring& s)
+{
+	std::wstring cpy(L"");
+	for (size_t i{ 0 }; i < s.size(); i += 2)
+		cpy += s[i] | (s[i + 1] << 8);
+	s = cpy;
+	return s;
+}
+
 class TinyIni
 {
 private:
-	template<typename charType>
-	static Encoding ConsumeBOM(std::basic_ifstream<charType>& stream)
-	{
-		charType c = stream.peek();
-		if (c == 0xff) stream.get();
-		c = stream.peek();
-		if (c == 0xfe)
-		{
-			stream.get();
-			return UTF16LE;
-		}
-		return DEFAULT;
-	}
-	
-	static std::wstring& utf16le_to_utf8(std::wstring& s)
-	{
-		std::wstring cpy(L"");
-		for (size_t i{ 0 }; i < s.size(); i += 2)
-			cpy += s[i] | (s[i + 1] << 8);
-		s = cpy;
-		return s;
-	}
-
 	static std::wstring& TrimString(std::wstring& s)
 	{
 		s.erase(s.begin(), std::find_if_not(s.begin(), s.end(), iswspace));
@@ -65,12 +70,14 @@ public:
 	using Sections = std::map<std::wstring, KeyValues>;
 	using IniFile = Sections;
 
-	static auto Read(const char *path)
+	static auto Read(const char *path, std::locale locale = std::locale("en_US.UTF-8"))
 	{
 		Sections sections;
 		std::wstring currentSection;
 		if (std::wifstream ifs(path, std::ios::binary); ifs)
 		{
+			std::ios_base::sync_with_stdio(false);
+			ifs.imbue(locale);
 			Encoding encoding = ConsumeBOM(ifs);
 			std::wstring line;
 			while (std::getline(ifs, line))
@@ -78,7 +85,7 @@ public:
 				if (line.empty() || std::all_of(line.begin(), line.end(), [](wchar_t c) { return iswspace(c) || c == 0; })) 
 					continue;
 				if (line.front() == 0) line.erase(line.begin());		
-				if (encoding == UTF16LE) utf16le_to_utf8(line);
+				if (encoding != DEFAULT) utf16le_to_utf8(line);
 				TrimString(line);
 				if (line.front() == L';') continue;
 				if (line.front() == L'[')
