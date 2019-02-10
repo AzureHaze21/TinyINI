712c3701cc674a567ecf24ee63a8ce229d153a90
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2019-02-10 13:19:16 +0100

    Update TinyINI.h

--- TinyINI.h
+++ TinyINI.h
@@ -6,51 +6,58 @@
 #include <map>
 #include <algorithm>
 
-enum Encoding
-{
-	DEFAULT,
-	UTF8,
-	UTF16LE,
-	UTF16BE,
-	UTF32LE,
-	UTF32BE,
-	UTF1,
-	UTFEBCDIC,
-	SCSU,
-	BOCU1,
-	GB18030
-};
-
-template<typename charType>
-Encoding ConsumeBOM(std::basic_ifstream<charType>& stream)
-{
-	unsigned char BOM[4]{ 0 };
-	charType c = 0;
-	int i = 0;
-	while (stream && (i < sizeof(BOM)))
-	{
-		BOM[i++] = stream.peek();
-		stream.get();
-		if (BOM[0] == 0xFF && BOM[1] == 0xFE) return UTF16LE;
-		if (BOM[0] == 0xFE && BOM[1] == 0xFF) return UTF16BE;
-		if (BOM[0] == 0xEF && BOM[1] == 0xBB && BOM[2] == 0xBF) return UTF8;
-	}
-	stream.seekg(0, std::ios::beg);
-	return DEFAULT;
-}
-
-std::wstring& utf16le_to_utf8(std::wstring& s)
-{
-	std::wstring cpy(L"");
-	for (size_t i{ 0 }; i < s.size(); i += 2)
-		cpy += s[i] | (s[i + 1] << 8);
-	s = cpy;
-	return s;
-}
-
 class TinyIni
 {
+public:
+	using KeyValues = std::map<std::wstring, std::wstring>;
+	using Sections = std::map<std::wstring, KeyValues>;
 private:
+	Sections sections;
+
+	TinyIni(Sections _sections) : sections(_sections) {}
+
+	enum Encoding
+	{
+		DEFAULT,
+		UTF8,
+		UTF16LE,
+		UTF16BE,
+		UTF32LE,
+		UTF32BE,
+		UTF1,
+		UTFEBCDIC,
+		SCSU,
+		BOCU1,
+		GB18030
+	};
+
+	template<typename charType>
+	static Encoding ConsumeBOM(std::basic_ifstream<charType>& stream)
+	{
+		unsigned char BOM[4]{ 0 };
+		charType c = 0;
+		int i = 0;
+		while (stream && (i < sizeof(BOM)))
+		{
+			BOM[i++] = stream.peek();
+			stream.get();
+			if (BOM[0] == 0xFF && BOM[1] == 0xFE) return UTF16LE;
+			if (BOM[0] == 0xFE && BOM[1] == 0xFF) return UTF16BE;
+			if (BOM[0] == 0xEF && BOM[1] == 0xBB && BOM[2] == 0xBF) return UTF8;
+		}
+		stream.seekg(0, std::ios::beg);
+		return DEFAULT;
+	}
+
+	static std::wstring& utf16le_to_utf8(std::wstring& s)
+	{
+		std::wstring cpy(L"");
+		for (size_t i{ 0 }; i < s.size(); i += 2)
+			cpy += s[i] | (s[i + 1] << 8);
+		s = cpy;
+		return s;
+	}
+
 	static std::wstring& TrimString(std::wstring& s)
 	{
 		s.erase(s.begin(), std::find_if_not(s.begin(), s.end(), iswspace));
@@ -66,10 +73,6 @@ private:
 		return  std::pair<std::wstring, std::wstring>{ key, val };
 	}
 public:
-	using KeyValues = std::map<std::wstring, std::wstring>;
-	using Sections = std::map<std::wstring, KeyValues>;
-	using IniFile = Sections;
-
 	static auto Read(const char *path, std::locale locale = std::locale(""))
 	{
 		Sections sections;
@@ -82,12 +85,12 @@ public:
 			std::wstring line;
 			while (std::getline(ifs, line))
 			{
-				if (line.empty() || std::all_of(line.begin(), line.end(), [](wchar_t c) { return iswspace(c) || c == 0; })) 
+				if (line.empty() || std::all_of(line.begin(), line.end(), [](wchar_t c) { return iswspace(c) || c == 0; }))
 					continue;
-				if (line.front() == 0) line.erase(line.begin());		
+				if (line.front() == 0) line.erase(line.begin());
 				if (encoding != DEFAULT) utf16le_to_utf8(line);
 				TrimString(line);
-				if (line.front() == L';' || line.front() == L'#') continue;
+				if (line.front() == L';' || line.front() == '#') continue;
 				if (line.front() == L'[')
 				{
 					if (line.back() != L']') continue;
@@ -106,6 +109,21 @@ public:
 				}
 			}
 		}
-		return sections;
+		return TinyIni(sections);
+	}
+
+	KeyValues get(const wchar_t *section)
+	{
+		return (section == nullptr ? KeyValues{} : sections[section]);
+	}
+
+	std::wstring get(const wchar_t *section, const wchar_t *key)
+	{
+		return key == nullptr ? L"" : get(section)[key];
+	}
+
+	KeyValues operator[](const wchar_t *index)
+	{
+		return (sections.count(index) > 0 ? sections[index] : KeyValues{});
 	}
 };
